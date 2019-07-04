9e490582c3ab88cd8b03170d73c9139120856401
Adam Power <apower@pivotal.io>
2019-07-04 15:29:51 +0100

    Formatting ELB config

--- lb.tf
+++ lb.tf
@@ -1,9 +1,8 @@
 resource "aws_elb" "k8s_masters" {
-  name = "k8s-master-${var.cluster_name}"
-
-  subnets = "${var.subnet_ids}"
-
+  name            = "k8s-master-${var.cluster_name}"
+  subnets         = "${var.subnet_ids}"
   security_groups = ["${var.master_security_group}"]
+  instances       = "${var.master_ids}"
 
   listener {
     instance_port     = 8443
@@ -13,12 +12,10 @@ resource "aws_elb" "k8s_masters" {
   }
 
   health_check {
-    target = "tcp:8443"
-    timeout = 5
-    interval = 30
+    target              = "tcp:8443"
+    timeout             = 5
+    interval            = 30
     unhealthy_threshold = 2
-    healthy_threshold = 10
+    healthy_threshold   = 10
   }
-
-  instances = "${var.master_ids}"
 }
