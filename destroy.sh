2cdc8549260f0b08ea0e32c9b16a56800f6e8f31
Adam Power <apower@pivotal.io>
2019-07-05 09:48:25 +0100

    Adding a destroy script

---
+++ destroy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+script_dir="$(cd "$(dirname "$0")"; pwd -P)"
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ${0} <cluster name>"
+  exit 1
+fi
+
+cluster_name="$1"
+
+export TF_VAR_cluster_name="$cluster_name"
+export TF_VAR_cluster_external_dns="$(pks cluster --json "$cluster_name" \
+  | jq -r '.parameters.kubernetes_master_host')"
+export TF_VAR_cluster_uuid="$(pks cluster --json "$cluster_name" \
+  | jq -r '.uuid')"
+
+terraform destroy --auto-approve
