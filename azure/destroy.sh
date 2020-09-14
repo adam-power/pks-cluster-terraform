571d42af9a38052d7dcfbf0600a5802ca924bbc0
Adam Power <apower@pivotal.io>
2020-09-14 16:00:08 +0100

    Deleting references to common.sh

--- azure/destroy.sh
+++ azure/destroy.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-script_dir="$(cd "$(dirname "$0")"; pwd -P)"
-
-# Shellcheck can't follow this file for some reason
-# shellcheck disable=SC1090
-. "${script_dir}/common.sh"
-
 if [ "$#" -ne 1 ]; then
   echo "Usage: ${0} <cluster name>"
   exit 1
